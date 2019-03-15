c6e964eefc425fff8168c9d50ea2b4306f1e00c7
efrain583 <efrain583@yahoo.com>
2019-03-14 23:15:08 -0400

    Made the log4j conf. dynamic

--- WebService/src/test/java/WebService/WebService/WSH.java
+++ WebService/src/test/java/WebService/WebService/WSH.java
@@ -1,6 +1,10 @@
 package WebService.WebService;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.apache.log4j.PropertyConfigurator;
 import org.json.JSONObject;
 
@@ -12,8 +16,25 @@ public class WSH {
 	public static void initTest(String suiteName) {
 		
 		//System.setProperty("com.jcabi.log.coloring", "true");
+//		PropertyConfigurator.configure("C:\\Users\\efrain583\\dev\\WebService\\src\\test\\java\\WebService\\WebService\\log4j.properties");
 		logger = Logger.getLogger(suiteName);
-		PropertyConfigurator.configure("C:\\Users\\efrain583\\dev\\WebService\\src\\test\\java\\WebService\\WebService\\log4j.properties");
+		Logger.getRootLogger().getLoggerRepository().resetConfiguration();
+		Logger rLogger = Logger.getRootLogger();
+		rLogger.setLevel(Level.DEBUG);
+		PatternLayout genericPatternLayout = new PatternLayout("%t %d{yyyy-MM-dd HH:mm:ss.SSS} %-5p %c %x - %m%n");
+
+		ConsoleAppender consoleAppender = new ConsoleAppender();
+		consoleAppender.setLayout(genericPatternLayout);
+		consoleAppender.activateOptions();
+		
+		FileAppender  fileAppender = new FileAppender();
+		fileAppender.setName("FileLogger");
+		fileAppender.setFile("WebService.log");
+		fileAppender.setLayout(genericPatternLayout);
+		fileAppender.activateOptions();
+		
+		rLogger.addAppender(consoleAppender);
+		rLogger.addAppender(fileAppender);
 	}
 	
 	static public boolean jsonParseSuccess(JSONObject jso) {
